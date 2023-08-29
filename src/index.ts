47d5ccd0b0c7eb635a1c58dbdb020ddf3eb6b554
Tim <9441755+tjc-dev@users.noreply.github.com>
2023-08-29 13:33:05 -0500

    feat: update to allow for mainnet/devnet

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ export default {
 
 		const { pathname, search } = new URL(request.url)
 		const payload = await request.text();
-		const proxyRequest = new Request(`https://${pathname === '/' ? 'mainnet.helius-rpc.com' : 'api.helius.xyz'}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
+		const proxyRequest = new Request(`https://${pathname === '/' ? '${env.HELIUS_ENV}.helius-rpc.com' : 'api.helius.xyz'}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
 			method: request.method,
 			body: payload || null,
 			headers: {
